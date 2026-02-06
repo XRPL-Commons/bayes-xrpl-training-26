87af54c11855260842e72398fff8288e5d9e0ee8
Mathis Sergent <sergent.mathis@gmail.com>
2026-02-06 11:33:14 +0100

    welcome

--- payment.py
+++ payment.py
@@ -1,5 +1,9 @@
 from xrpl.clients import JsonRpcClient
 from xrpl.wallet import generate_faucet_wallet
+from xrpl.account import get_balance
+from xrpl.utils import xrp_to_drops, drops_to_xrp
+from xrpl.models.transactions import Payment
+from xrpl.transaction import submit_and_wait
 
 def payment():
     print("Let's make payments... 💰")
--- tokens.py
+++ tokens.py
@@ -1,2 +1,16 @@
+from xrpl.clients import JsonRpcClient
+from xrpl.wallet import generate_faucet_wallet
+from xrpl.account import get_balance
+from xrpl.utils import xrp_to_drops, drops_to_xrp
+from xrpl.models.transactions import AccountSet, TrustSet, AMMCreate
+from xrpl.models.transactions.account_set import AccountSetAsfFlag
+from xrpl.models.amounts import IssuedCurrencyAmount
+from xrpl.transaction import submit_and_wait
+
+def convert_string_to_hex_padded(s):
+    hex_str = s.encode().hex()
+    return hex_str.ljust(40, "0").upper()
+
+
 def tokens():
     print("Let's manage tokens... 📊")

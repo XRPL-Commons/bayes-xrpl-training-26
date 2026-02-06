c6ec12c6147a7cac39c0b93596d13afc93390f93
Mathis Sergent <sergent.mathis@gmail.com>
2026-02-06 13:34:00 +0100

    prepare payment, sign, submit, and verify

--- payment.py
+++ payment.py
@@ -1,3 +1,4 @@
+from re import sub
 from xrpl.clients import JsonRpcClient
 from xrpl.wallet import generate_faucet_wallet
 from xrpl.account import get_balance
@@ -18,8 +19,24 @@ def payment():
 
     # Display sender info
     print(f"Sender Address: {sender_wallet.classic_address}")
-    print(f"Sender Balance: {drops_to_xrp(sender_balance)} XRP")
+    print(f"Sender Balance: {drops_to_xrp(str(sender_balance))} XRP")
 
     # Display receiver info
     print(f"Receiver Address: {receiver_wallet.classic_address}")
-    print(f"Receiver Balance: {drops_to_xrp(receiver_balance)} XRP")
+    print(f"Receiver Balance: {drops_to_xrp(str(receiver_balance))} XRP")
+
+    # Prepare the transaction
+    payment_tx = Payment(
+        account=sender_wallet.address,
+        destination=receiver_wallet.address,
+        amount=xrp_to_drops(10)
+    )
+
+    # Sign and submit the transaction
+    payment_result = submit_and_wait(payment_tx, client, sender_wallet, autofill=True)
+
+    # Verify the result
+    if payment_result.is_successful():
+        print("✅ Payment tx succeeded: ", payment_result.result['hash'])
+    else:
+        print("❌ Payment tx failed: ", payment_result.result)

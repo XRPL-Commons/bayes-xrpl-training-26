5b59581518172ea4aa2dfe8c8f87612713d417cb
Mathis <72806895+mathisrgt@users.noreply.github.com>
2026-03-12 13:15:24 +0000

    account set and trustset

--- tokens.py
+++ tokens.py
@@ -14,4 +14,54 @@ def convert_string_to_hex_padded(s):
 def tokens():
     client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
 
-    # Let's use DeFi!
+    # Create wallets and fund them
+    issuer_wallet = generate_faucet_wallet(client)
+    holder_wallet = generate_faucet_wallet(client)
+
+    # Fetch balances
+    issuer_balance = get_balance(issuer_wallet.address, client)
+    holder_balance = get_balance(holder_wallet.address, client)
+
+    # Display issuer info
+    print(f"Issuer Address: {issuer_wallet.classic_address}")
+    print(f"Issuer Balance: {drops_to_xrp(str(issuer_balance))} XRP")
+
+    # Display holder info
+    print(f"Holder Address: {holder_wallet.classic_address}")
+    print(f"Holder Balance: {drops_to_xrp(str(holder_balance))} XRP")
+
+    # ACCOUNT SET
+    account_set_tx = AccountSet(
+        account=issuer_wallet.address,
+        set_flag=AccountSetAsfFlag.ASF_DEFAULT_RIPPLE
+    )
+
+    account_set_result = submit_and_wait(account_set_tx, client, issuer_wallet)
+
+    # Verify the result
+    if account_set_result.is_successful():
+        print("✅ Account Set tx succeeded: ", account_set_result.result['hash'])
+    else:
+        print("❌ Account Set tx failed: ", account_set_result.result)
+
+    currency_hex = convert_string_to_hex_padded("BAYES")
+
+    # TRUSTLINE
+    trust_set_tx = TrustSet(
+        account=holder_wallet.address,
+        limit_amount=IssuedCurrencyAmount(
+                currency=currency_hex,
+                issuer=issuer_wallet.address,
+                value="1000",
+            )
+        )
+
+    trust_set_result = submit_and_wait(trust_set_tx, client, holder_wallet)
+
+    # Verify the result
+    if trust_set_result.is_successful():
+        print("✅ TrustSet tx succeeded: ", trust_set_result.result['hash'])
+    else:
+        print("❌ TrustSet tx failed: ", trust_set_result.result)
+
+
